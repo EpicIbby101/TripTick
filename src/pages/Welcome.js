6221bd6db80e120c64b4a43ae59539c667726dce
EpicIbby101 <kevin_grittner@outlook.com>
2023-05-22 22:25:38 +0100

    Added a landing page and signin function

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -9,11 +9,11 @@ function Welcome() {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center h-screen bg-gradient-to-b from-sky-400 to-sky-200">
-      <h1 className="text-3xl md:text-5xl sm:text-4xl font-bold mb-2">Welcome to TripTick</h1>
-      <p className="text-lg mb-5">Your all-in-one travel companion</p>
+    <main className="flex flex-col justify-center items-center h-screen bg-gradient-to-b from-sky-400 to-sky-200">
+      <h1 className="text-3xl md:text-5xl sm:text-4xl font-bold mb-2">Welcome to TripTicker</h1>
+      <p className="text-lg mb-5">Your essential travel companion</p>
       <div className="relative group">
-        <div className="absolute -inset-1.5 bg-gradient-to-tr from-green-400 to-green-600 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-200"></div>
+        <div className="absolute -inset-1.5 bg-gradient-to-tr from-gray-200 to-gray-300 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-200"></div>
       <button
         onClick={handleSignIn}
         className="bg-black relative text-white font-bold py-3 px-10 rounded group-hover:text-gray-100 transition duration-200"
@@ -21,7 +21,7 @@ function Welcome() {
         Sign In
       </button>
       </div>
-    </div>
+    </main>
   );
 }
 export default Welcome;
