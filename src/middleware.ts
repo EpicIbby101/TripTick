97cb802d6cf8d385de7c33f2c3342d590c6d98e2
EpicIbby101 <kevin_grittner@outlook.com>
2023-05-23 16:32:49 +0100

    Potential vercel deployment fixes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware();
+export default authMiddleware({
+    publicRoutes: ["/Welcome"]
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,7 +5,12 @@ import { ClerkProvider, SignedIn, SignedOut } from "@clerk/nextjs";
 export default function App({ Component, pageProps }) {
   return (
     <ClerkProvider {...pageProps}>
+      <SignedIn>
       <Component {...pageProps} />
+      </SignedIn>
+      <SignedOut>
+        <SignIn />
+      </SignedOut>
     </ClerkProvider>
   );
 }
