6e880efc8ba3f3f86fb6ae67e7a9ea3a45b16328
EpicIbby101 <kevin_grittner@outlook.com>
2023-07-26 22:12:08 +0100

    Added custom items feature

--- src/pages/EssentialsChecklist.js
+++ src/pages/EssentialsChecklist.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import ChecklistData from "@/components/ChecklistData";
-import ArrowCircleDownIcon from '@mui/icons-material/ArrowCircleDown';
-import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp';
+import ArrowCircleDownIcon from "@mui/icons-material/ArrowCircleDown";
+import ArrowCircleUpIcon from "@mui/icons-material/ArrowCircleUp";
 
 const EssentialsChecklist = () => {
   const [checkedItems, setCheckedItems] = useState([]);
   const [showCategory, setShowCategory] = useState({});
+  const [customItem, setCustomItem] = useState("");
 
   useEffect(() => {
     // Check if localStorage is available
@@ -31,14 +32,19 @@ const EssentialsChecklist = () => {
 
   const handleItemCheck = (index) => {
     const updatedCheckedItems = [...checkedItems];
+
+    // If the index exists in the checkedItems array, remove it
+    // This is for pre-made items
     if (updatedCheckedItems.includes(index)) {
       updatedCheckedItems.splice(updatedCheckedItems.indexOf(index), 1);
     } else {
+      // If the index is not found, add it
+      // This is for custom items (index will be an object)
       updatedCheckedItems.push(index);
     }
     setCheckedItems(updatedCheckedItems);
 
-    //save to localStorage (only if available)
+    // Save to localStorage (only if available)
     if (typeof window !== undefined) {
       localStorage.setItem(
         "selectedItems",
@@ -58,6 +64,17 @@ const EssentialsChecklist = () => {
         itemName: item.item,
       });
     });
+    checkedItems.forEach((item, index) => {
+        if (!groupedItems["Custom Items"]) {
+          groupedItems["Custom Items"] = [];
+        }
+        if (item.custom) {
+          groupedItems["Custom Items"].push({
+            index,
+            itemName: item.itemName,
+          });
+        }
+      });
     return groupedItems;
   };
 
@@ -71,10 +88,12 @@ const EssentialsChecklist = () => {
   };
 
   return (
-    <div className="bg-white shadow-md rounded-lg p-4 max-w-md mx-auto mt-24">
-        <div className="flex items-center justify-center mb-4">
-        <h2 className="text-black text-2xl font-semibold">📃Essentials Checklist📃</h2>
-        </div>
+    <div className="bg-white shadow-md rounded-lg p-4 max-w-md mx-auto mt-10">
+      <div className="flex items-center justify-center mb-4">
+        <h2 className="text-black text-2xl font-semibold">
+          📃Essentials Checklist📃
+        </h2>
+      </div>
       {Object.entries(groupedItems).map(([category, items]) => (
         <div key={category} className="mb-4">
           <button
@@ -82,8 +101,12 @@ const EssentialsChecklist = () => {
             className="btn-primary mb-2 px-2 py-0.5 flex items-center w-full justify-between bg-gray-600 rounded-md"
           >
             <div className="flex items-center justify-between">
-            <span>{category}</span>
-            {showCategory[category] ? ( <ArrowCircleUpIcon style={{ marginLeft: '8px' }}/> ) : ( <ArrowCircleDownIcon style={{ marginLeft: '8px' }}/> )}
+              <span>{category}</span>
+              {showCategory[category] ? (
+                <ArrowCircleUpIcon style={{ marginLeft: "8px" }} />
+              ) : (
+                <ArrowCircleDownIcon style={{ marginLeft: "8px" }} />
+              )}
             </div>
           </button>
           {showCategory[category] && (
@@ -94,7 +117,13 @@ const EssentialsChecklist = () => {
                     type="checkbox"
                     id={`item-${index}`}
                     className="mr-2"
-                    checked={checkedItems.includes(index)}
+                    checked={
+                      typeof index === "number"
+                        ? checkedItems.includes(index)
+                        : checkedItems.some(
+                            (item) => item.custom && item.itemName === itemName
+                          )
+                    }
                     onChange={() => handleItemCheck(index)}
                   />
                   <label htmlFor={`item-${index}`}>{itemName}</label>
@@ -104,13 +133,48 @@ const EssentialsChecklist = () => {
           )}
         </div>
       ))}
+      <div>
+        <div className="flex mb-3">
+        <input
+          type="text"
+          value={customItem}
+          onChange={(e) => setCustomItem(e.target.value)}
+          placeholder="Enter custom item"
+          className="border border-gray-300 rounded-md p-2 px-10 mr-2 mb-3 text-black flex-grow"
+        />
+        <button
+          onClick={() => {
+            // Add the custom item to the checklist
+            if (customItem.trim() !== "") {
+              const updatedItems = [
+                ...checkedItems,
+                { custom: true, itemName: customItem },
+              ];
+              setCheckedItems(updatedItems);
+              setCustomItem("");
+
+              // Save the updated checklist to localStorage (only if available)
+              if (typeof window !== undefined) {
+                localStorage.setItem(
+                  "selectedItems",
+                  JSON.stringify(updatedItems)
+                );
+              }
+            }
+          }}
+          className="bg-green-500 px-5 py-1 rounded-md hover:bg-green-700 w-full btn-secondary"
+        >
+          Add Custom Item
+        </button>
+        </div>
+      </div>
       <div className="flex justify-between">
-      <button
-        onClick={handleClearAll}
-        className="bg-blue-500 px-5 py-2 rounded-md hover:bg-blue-700 w-full btn-secondary"
-      >
-       🗑️ Clear All 🗑️
-      </button>
+        <button
+          onClick={handleClearAll}
+          className="bg-blue-500 px-5 py-2 rounded-md hover:bg-blue-700 w-full btn-secondary"
+        >
+          🗑️ Clear All 🗑️
+        </button>
       </div>
     </div>
   );
